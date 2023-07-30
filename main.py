b1f3658d89e874794e8c652d34e9a863f03096fc
David Bradburn <david.bradburn2@gmail.com>
2023-07-30 13:43:01 +0100

    Removed recursive main menu call - Might want to look into gui and user interfaces Removed legacy code for member creation Fixed a bug that allowed previous stale inputs to be remembered and used

--- main.py
+++ main.py
@@ -6,23 +6,13 @@ import os
 class rowing_member():
     
     def __init__(self):
-        self.create_member()
+        self.enter_member_name()
         self.enter_side()
-        # self.enter_sculling()
-        # self.enter_foot_steering()
-
-        # if self.can_scull == "yes":
-        #     self.enter_1x_proficiency() 
-        # else:
-        #     self.can_1x = "no"
-
-        # self.enter_coxing_ability()
-
         self.display_member()
 
 
-    def create_member(self):
-        self.mem_name = input("Enter member name (i.e. first last): ")
+    def enter_member_name(self):
+        self.mem_name = str(input("Enter member name (i.e. first last): "))
 
 
     # def enter_data(self, msg: str, options: list):
@@ -43,46 +33,6 @@ class rowing_member():
             case _:
                 print("Please enter valid side (stroke, bow, either or n/a): ")
                 self.enter_side()
-
-
-    def enter_sculling(self):
-        temp = input(f"Can {self.mem_name} scull (yes/no): ")
-        match temp:
-            case "yes" | "no":
-                self.can_scull = temp
-            case _:
-                print("Please enter a valid state (yes or no): ")
-                self.enter_sculling()
-
-
-    def enter_foot_steering(self):
-        temp = input(f"Can {self.mem_name} foot steer (yes/no): ")
-        match temp:
-            case "yes" | "no":
-                self.can_foot_steer = temp
-            case _:
-                print("Please enter a valid state (yes or no): ")
-                self.enter_foot_steering()
-
-
-    def enter_1x_proficiency(self):
-        temp = input(f"Is {self.mem_name} confident in a 1x (yes/no): ")
-        match temp:
-            case "yes" | "no":
-                self.can_1x = temp
-            case _:
-                print("Please enter a valid state (yes or no): ")
-                self.enter_1x_proficiency()
-
-
-    def enter_coxing_ability(self):
-        temp = input(f"Can {self.mem_name} cox (yes/no): ")
-        match temp:
-            case "yes" | "no":
-                self.can_cox = temp
-            case _:
-                print("Please enter a valid state (yes or no): ")
-                self.enter_coxing_ability()
 
 
     def check_if_file_exists(self, path, filename):
@@ -123,7 +73,7 @@ class rowing_member():
             file_exists = self.check_if_file_exists(base, filename)
             if(file_exists):
                 print(f"filename {self.mem_name} already exists please rename")
-                self.create_member()
+                self.enter_member_name()
                 self.save_object()
                 return
 
@@ -140,14 +90,19 @@ class outing_planner():
 
     def __init__(self):
 
-        self.main_menu()
+        self.main()
+    
+    def main(self):
+
+        while True:
+            self.main_menu()
 
 
     def main_menu(self):
         print("------------------------------------------------------")
 
         try: 
-            op = int(input("Please enter operation: \n   0 : create and outing\n   1 : create member\n   2 : view / edit member \n   9 : quit program\n"))
+            op = int(input("Please enter operation: \n   0 : create and outing\n   1 : create member\n   2 : load member \n   3 : edit loaded member \n   9 : quit program\n"))
 
 
             match op:
@@ -159,11 +114,12 @@ class outing_planner():
                     obj = rowing_member()
                     obj.save_object()
                 case 2: # view /edit member // load and edit
-                    mem_filename = input("Please enter the member you wish to view / make a change: ")
+                    mem_name = input("Please enter the member you wish to view / make a change: ")
                     # print(type(mem_filename))
-                    temp_name = f"{mem_filename}.pickle"
+                    # temp_name = f"{mem_filename}.pickle"
                     # print(temp_name)
-                    obj2 = self.load_object(temp_name)
+                    obj2 = self.load_object(mem_name)
+
                     obj2.display_member()
 
 
@@ -175,27 +131,25 @@ class outing_planner():
 
         except ValueError:
             print("Please enter valid value")
-            self.main_menu()
-
-
-        self.main_menu()
-
+            
 
     def load_object(self, filename):
 
         dir_path = os.path.dirname(os.path.realpath(__file__))
 
         root = "member_data"
-        filename = f"{self.mem_name}.pickle"
+        filename_ext = f"{filename}.pickle"
 
         base = os.path.join(dir_path, root)
-        filepath = os.path.join(base, filename)
+        filepath = os.path.join(base, filename_ext)
 
+        print(filepath)
         try:
             with open(filepath, "rb") as f:
-                return pickle.load(f)
+                return pickle.load(f), True
         except Exception as ex:
             print("Error during unpickling object (Possibly unsupported):", ex)
+            return None, False
 
 
 outing_planner()
