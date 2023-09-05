8537ffa3308e34a9fb2bdfc3111f86b37d3902b8
David Bradburn <david.bradburn2@gmail.com>
2023-09-05 17:48:30 +0100

    creation feature for json data

--- main.py
+++ main.py
@@ -34,13 +34,15 @@ class outing_planner():
                 case 1: # Create a member
                     member_name = str(input("Please enter member name"))
                     member_side = str(input(f"Please enter {member_name}'s rowing side"))
-                    member_ID   = hash(time.time())
+                    member_ID   = hash(time.time()) ## might want to think of a better userid method?
                     print(member_ID)
                     member_dict = {"Name":member_name,
                                    "Side":member_side,
                                    "ID"  :member_ID}
 
                     print(member_dict)
+                    self.save_member(member_dict)
+
 
                 case 2: # view /edit member // load and edit
                     mem_name = input("Please enter the member you wish to view : ")
@@ -79,6 +81,25 @@ class outing_planner():
         except Exception as ex:
             print("Error during loading object (possiblt does not exist):", ex)
             return None
+    
+    def save_member(self, file: dict):
+
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+
+        root = "member_data"
+        filename = file["Name"]
+        filename_ext = f"{filename}.json"
+
+        base = os.path.join(dir_path, root)
+        filepath = os.path.join(base, filename_ext)
+
+        print(filepath)
+
+        try:
+            with open(filepath, "w") as outfile:
+                json.dump(file, outfile)
+        except Exception as ex:
+            print("Error during saving object (possiblt does not exist):", ex)
 
 
 outing_planner()
