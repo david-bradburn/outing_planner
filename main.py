c86b93aebec00322c415eb8084396d4a905ea200
David Bradburn <david.bradburn2@gmail.com>
2023-10-15 16:25:53 +0100

    changed member file from pickle to json

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-import pickle
 import os
 import rowing_member
-# from enum import Enum
+import json
+import time
+
 
 
 
 class outing_planner():
 
     def __init__(self):
-
+        # self.loaded_member
+        
         self.main()
     
     def main(self):
-
         while True:
             self.main_menu()
 
@@ -21,32 +22,32 @@ class outing_planner():
         print("------------------------------------------------------")
 
         try: 
-            op = int(input("Please enter operation: \n   0 : create and outing\n   1 : create member\n   2 : display/edit member \n   9 : quit program\n"))
+            op = int(input("Please enter operation: \n   0 : create and outing\n   2 : display member \n   9 : quit program\n"))
 
 
             match op:
-
-                case 0: # Create outing
+                case 0:
+                    # Create outing
                     ...
+                
+                case 1: # Create a member
+                    member_name = str(input("Please enter member name : "))
+                    member_side = str(input(f"Please enter {member_name}'s rowing side: "))
+                    member_ID   = int(time.time()) ## might want to think of a better userid method? nah this is fine
+                    # print(member_ID)
+                    member_dict = {"Name":member_name,
+                                   "Side":member_side,
+                                   "ID"  :member_ID,
+                                   "History" : []}
 
-                case 1: # create_member + save
-                    obj = rowing_member.rowing_member()
-                    obj.save_object()
+                    # print(member_dict)
+                    self.save_member(member_dict)
+
+
                 case 2: # view /edit member // load and edit
-                    mem_name = input("Please enter the member you wish to view / make a change: ")
-                    self.loaded_mem_obj, valid_mem = self.load_object(mem_name)
-
-                    if not valid_mem:
-                        print("Error in loading")
-                        return 
-                    
-                    self.loaded_mem_obj.display_member()
-                    edit_mem = self.edit_member_ask()
-                    if edit_mem:
-                        self.edit_member()
-                    
-
-
+                    mem_name = input("Please enter the member you wish to view : ")
+                    self.loaded_member_raw = self.load_member(mem_name)
+                    self.loaded_member = rowing_member.rowing_member(self.loaded_member_raw)
 
                 case 9:
                     quit()
@@ -54,72 +55,46 @@ class outing_planner():
                 case _:
                     raise ValueError
 
-        except ValueError:
-            print("Please enter valid value")
-
-    def edit_member_ask(self):
-        ip = str(input(f"Would you like to edit member ({self.loaded_mem_obj.mem_name})? y/n: "))
-        try:
-            match ip.lower():
-                case "n":
-                    return False
-                case "y":
-                    return True
-                case _:
-                    raise ValueError
-        except ValueError:
-            print("Please enter a valid value")
-            return self.edit_member_ask()
-            
-    def edit_member(self):
-        ip = int(input("Please enter which element to edit: \n   0 : name\n   1 : side\n   9 : save & exit\n"))
-
-        name_changed = False
-        try:
-            match ip:
-                case 0:
-                    self.old_name = self.loaded_mem_obj.mem_name
-                    self.loaded_mem_obj.enter_member_name()
-                    name_changed = True
-
-                    self.edit_member()
-                case 1:
-                    self.loaded_mem_obj.enter_side()
-                    self.edit_member()
-                case 9:
-                    self.loaded_mem_obj.save_object(name_changed)
-                    if name_changed:
-                        self.loaded_mem_obj.del_object(self.old_name)
-
-                case _:
-                    raise ValueError
-
-        except ValueError:
-            print("Please enter a valid value 0 or 1")   
-            self.edit_member()
-        
+        except ValueError as ex:
+            print("Please enter valid value", ex)
 
 
     def create_outing(self):
         ...
 
-    def load_object(self, filename):
-
+    def save_member(self, member_data: dict):
         dir_path = os.path.dirname(os.path.realpath(__file__))
 
         root = "member_data"
-        filename_ext = f"{filename}.pickle"
+        filename_ext = f"{member_data['Name']}.json"
+
+        base = os.path.join(dir_path, root)
+        filepath = os.path.join(base, filename_ext)
+        json_object = json.dumps(member_data, indent=4)
+
+        print(filepath)
+        with open(filepath, "w") as outfile:
+            outfile.write(json_object)
+
+
+    def load_member(self, filename):
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+
+        root = "member_data"
+        filename_ext = f"{filename}.json"
 
         base = os.path.join(dir_path, root)
         filepath = os.path.join(base, filename_ext)
 
         print(filepath)
+
         try:
-            with open(filepath, "rb") as f:
-                return pickle.load(f), True
+            with open(filepath, 'r') as f:
+                return json.load(f)
         except Exception as ex:
-            print("Error during unpickling object (Possibly unsupported):", ex)
-            return None, False
+            print("Error during loading object (possiblt does not exist):", ex)
+            return None
+    
 
 
 outing_planner()
--- rowing_member.py
+++ rowing_member.py
@@ -1,79 +1,50 @@
 import pickle
 import os
+import utils
 
 class rowing_member():
     
-    def __init__(self):
-        self.enter_member_name()
-        self.enter_side()
+    def __init__(self, dict):
+        ## Need to write functions to always source from dict to avoid lots of conversions
+        self.rowing_member_data = dict
+        self.name_changed = False
         self.display_member()
 
-
-    def enter_member_name(self):
-        self.mem_name = str(input("Enter member name (i.e. first last): "))
-
-
-    def enter_side(self): # sweep only
-        # self.mem_side 
-        temp = input(f"Enter {self.mem_name}'s rowing side (stroke, bow, either or n/a): ")
-        match temp:
-
-            case "stroke" | "bow" | "either" | "n/a":
-                self.mem_side = temp
-            case _:
-                print("Please enter valid side (stroke, bow, either or n/a): ")
-                self.enter_side()
-
-
-    def check_if_file_exists(self, path, filename):
-        """Checks to see if a given file exits at a given location"""
-        for root, dir1, files in os.walk(path):
-            if filename in files:
-                print(filename)
-                return True
-
-        # print("file does not exist")
-        return False
-
+    def get_member_name(self, exception:Exception = KeyError):
+        try:
+            return self.rowing_member_data["Name"]
+        except exception:
+            return exception
+    
+    def get_member_side(self, exception:Exception = KeyError):
+        try:
+            return self.rowing_member_data["Side"]
+        except exception:
+            return exception
+    
+    def get_member_id(self, exception:Exception = KeyError):
+        try:
+            return self.rowing_member_data["ID"]
+        except exception:
+            return exception
+    
+    def get_member_history(self, exception:Exception = KeyError):
+        """Unused for now"""
+        try:
+            return self.rowing_member_data["History"]
+        except exception:
+            return exception
 
     def display_member(self):
         """Just prints the rowing member attributes"""
-        print(f"Name        : {self.mem_name}")
-        print(f"Rowing side : {self.mem_side}")
+        print(f"Name        : {self.get_member_name()}")
+        print(f"Rowing side : {self.get_member_side()}")
+        print(f"ID          : {self.get_member_id()}")
+        print(f"History     : {self.get_member_history()}")
 
+    def set_member_name(self, new_name):
+        """Unused for now"""
+        self.old_member_name = self.rowing_member_data["Name"]
+        self.rowing_member_data["Name"] = new_name
+        print(f"old name {self.old_member_name} -> new name {new_name}")
 
-    def del_object(self, old_name):
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        root = "member_data"
-        filename = f"{old_name}.pickle"
-        base = os.path.join(dir_path, root)
-        filepath = os.path.join(base, filename)
-        print(filepath)
-        os.remove(filepath)
-        
-
-
-    def save_object(self, overwrite = False):
-
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-
-        root = "member_data"
-        filename = f"{self.mem_name}.pickle"
-
-        base = os.path.join(dir_path, root)
-        filepath = os.path.join(base, filename)
-
-        if(not overwrite):
-            # print(f"checking if file exitsts in {base}")
-            file_exists = self.check_if_file_exists(base, filename)
-            if(file_exists):
-                print(f"filename {self.mem_name} already exists please rename")
-                self.enter_member_name()
-                self.save_object()
-                return
-
-        try:
-            with open(filepath, "wb") as f:
-                pickle.dump(self, f, protocol=pickle.HIGHEST_PROTOCOL)
-        except Exception as ex:
-            print("Error during pickling object (Possibly unsupported):", ex)
