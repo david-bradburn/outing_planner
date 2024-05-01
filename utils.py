f6ff915928588dcd7794c1c25fb2042950040508
David Bradburn <david.bradburn2@gmail.com>
2024-05-01 13:53:16 +0100

    Merge of branch

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import csv
 
 def print_ex_catch(msg: str, error_type: Exception):
     "Doesn't work if the error occurs in the message string :("
@@ -5,4 +6,10 @@ def print_ex_catch(msg: str, error_type: Exception):
         print(msg)
     except:
         pass
+
+def write_to_csv(filename, data):
+  with open(f"{filename}.csv", "w") as fd:
+    wr = csv.writer(fd, quoting=csv.QUOTE_ALL)
+    for row in data:
+      wr.writerow(row)
 
